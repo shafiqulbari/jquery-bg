2a7f2921619a2afa759aec38c4919daf68cadfcb
Shafiqul Bari <120185849+shafiqulbari@users.noreply.github.com>
2023-09-16 17:09:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   //alert
+$('.salert').on( 'click', function(){
+    alert("You're Beautiful");
+})
+//showhHide
+$('#show').on('click', function(){
+    $('#grabber').show();
+})
+$('#hide').on('click', function(){
+    $('#grabber').hide();
+})
+
+
+});
+
